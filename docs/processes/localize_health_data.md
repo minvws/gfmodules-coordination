cb5cb4d724bc06d564210aa808dad494a5d936f3
Gerbrand Bosch <gerbrandb@gmail.com>
2024-10-02 09:56:07 +0200

    Documentation updates
    Use structurizr to create diagrams to documentated the supported processes
    
    Signed-off-by: Gerbrand Bosch <gerbrandb@gmail.com>

---
+++ docs/processes/localize_health_data.md
@@ -0,0 +1,20 @@
+# Localize health data
+
+## Summary
+
+This process enables healthcare professionals to use their EPD to localize the health data of a certain patient based
+on their BSN.
+
+## Process overview
+
+Diagram of the overview
+
+![Localize Health Data](../images/structurizr-LocalizeHealthData.svg "Localize Health Data")
+
+## Interface definitions
+
+2\. TODO: Request RID
+
+[3\. Localize health data with RID](https://github.com/minvws/gfmodules-national-referral-index/blob/main/docs/interface-definitions/lookup.md)
+
+4\. TODO: Request base pseudonym
--- tools/script.sh
+++ tools/script.sh
@@ -12,47 +12,48 @@ DEBUG=false
 
 # Function to display script usage
 function display_usage() {
-  echo "Usage: $0 [-h|--help] [-r|--remove] [-c|--clear-config] [-a|--autopilot] [-- command]"
-  echo "Options:"
-  echo "  -h, --help                      Display this help message"
-  echo "  -r, --remove                    Docker compose stop and remove"
-  echo "  -c, --clear-config              Remove the local config files"
-  echo "  -a, --autopilot                 Copy the env files from the example files in each repository"
-  echo "  -b  --build                     Rebuild the docker containers"
-  echo "  --                              Everything behind the -- is executed in every module"
-  exit 1
+	echo "Usage: $0 [-h|--help] [-r|--remove] [-c|--clear-config] [-a|--autopilot] [-- command]"
+	echo "Options:"
+	echo "  -h, --help                      Display this help message"
+	echo "  -r, --remove                    Docker compose stop and remove"
+	echo "  -c, --clear-config              Remove the local config files"
+	echo "  -a, --autopilot                 Copy the env files from the example files in each repository"
+	echo "  -b  --build                     Rebuild the docker containers"
+	echo "  -d  --debug                     Write debug log file"
+	echo "  --                              Everything behind the -- is executed in every module"
+	exit 1
 }
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
-  case $1 in
-  -h | --help)
-    display_usage
-    ;;
-  -r | --remove)
-    REMOVE=true
-    shift
-    ;;
-  -c | --clear-config)
-    CLEAR_CONFIG=true
-    shift
-    ;;
-  -a | --autopilot)
-    AUTOPILOT=true
-    shift
-    ;;
-  -b | --build)
-    BUILD=true
-    shift
-    ;;
-  -d | --debug)
-    DEBUG=true
-    shift
-    ;;
-  *)
-    shift
-    ;;
-  esac
+	case $1 in
+	-h | --help)
+		display_usage
+		;;
+	-r | --remove)
+		REMOVE=true
+		shift
+		;;
+	-c | --clear-config)
+		CLEAR_CONFIG=true
+		shift
+		;;
+	-a | --autopilot)
+		AUTOPILOT=true
+		shift
+		;;
+	-b | --build)
+		BUILD=true
+		shift
+		;;
+	-d | --debug)
+		DEBUG=true
+		shift
+		;;
+	*)
+		shift
+		;;
+	esac
 done
 
 SCRIPT=$(readlink -f $0)
@@ -63,65 +64,65 @@ cd $BASEDIR
 MODULES="nl-irealisatie-zmodules-pgo-demo nl-irealisatie-zmodules-addressing-register gfmodules-national-referral-index gfmodules-localization-register-service gfmodules-localization-metadata-register nl-irealisatie-zmodules-pseudonym-service nl-irealisatie-zmodules-qualification-register-api nl-irealisatie-zmodules-qualification-register-admin-api nl-irealisatie-zmodules-qualification-register-web"
 
 for module in $MODULES; do
-  if [ ! -d "$module" ]; then
-    git clone git@github.com:minvws/${module}.git
-  fi
+	if [ ! -d "$module" ]; then
+		git clone git@github.com:minvws/${module}.git
+	fi
 done
 
 if "$REMOVE" || "$CLEAR_CONFIG"; then
-  cd $BASEDIR/gfmodules-coordination
-  if "$REMOVE"; then
-    docker compose stop -t 0
-    docker compose rm -f
-  fi
+	cd $BASEDIR/gfmodules-coordination
+	if "$REMOVE"; then
+		docker compose stop -t 0
+		docker compose rm -f
+	fi
 
-  for module in $MODULES; do
-    cd $BASEDIR/${module}
-    echo "=== ${module} ==="
-    if "$REMOVE"; then
-      docker compose stop -t 0
-      docker compose rm -f
-    fi
-    if "$CLEAR_CONFIG"; then
-      rm app.conf || true
-      rm .env.local || true
-      rm .autopilot || true
-    fi
-    echo "==="
-  done
+	for module in $MODULES; do
+		cd $BASEDIR/${module}
+		echo "=== ${module} ==="
+		if "$REMOVE"; then
+			docker compose stop -t 0
+			docker compose rm -f
+		fi
+		if "$CLEAR_CONFIG"; then
+			rm app.conf || true
+			rm .env.local || true
+			rm .autopilot || true
+		fi
+		echo "==="
+	done
 fi
 
 if "$AUTOPILOT"; then
-  cd $BASEDIR/gfmodules-coordination
-  if "$BUILD"; then
-    docker compose build
-  fi
-  docker compose up -d --remove-orphans
-  for module in $MODULES; do
-    echo "=== Start ${module} with preconfigured autopilot configuration ==="
-    cd $BASEDIR/${module}
+	cd $BASEDIR/gfmodules-coordination
+	if "$BUILD"; then
+		docker compose build
+	fi
+	docker compose up -d --remove-orphans
+	for module in $MODULES; do
+		echo "=== Start ${module} with preconfigured autopilot configuration ==="
+		cd $BASEDIR/${module}
 
-    if [ -e tools/autopilot.sh ]; then
-      tools/./autopilot.sh
-    else
-      if "$BUILD"; then
-        docker compose build
-      fi
-      docker compose up -d --remove-orphans
-    fi
+		if [ -e tools/autopilot.sh ]; then
+			tools/./autopilot.sh
+		else
+			if "$BUILD"; then
+				docker compose build
+			fi
+			docker compose up -d --remove-orphans
+		fi
 
-    echo "==="
-  done
+		echo "==="
+	done
 fi
 
 if "$DEBUG"; then
-  cd $BASEDIR
-  find . -mindepth 1 -maxdepth 1 -type d -print -exec bash -c "cd {} && docker compose logs --tail=100" \; >$BASEDIR/debug.log
-  echo "Stored the debug file at $BASEDIR/debug.log"
+	cd $BASEDIR
+	find . -mindepth 1 -maxdepth 1 -type d -print -exec bash -c "cd {} && docker compose logs --tail=100" \; >$BASEDIR/debug.log
+	echo "Stored the debug file at $BASEDIR/debug.log"
 fi
 
 if "$AUTOPILOT"; then
-  cat <<"EOF"
+	cat <<"EOF"
 
 
  ______                    _       _
